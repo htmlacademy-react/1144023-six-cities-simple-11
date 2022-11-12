26400b48fe4bd4bf17242ca7a6e3a4d495736087
me <glawog@gmail.com>
2022-11-12 20:11:37 +0300

    fix

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -20,8 +20,12 @@ function ReviewForm(): JSX.Element {
     }
   }
 
+  function handleReviewFormSubmit(event: ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form className='reviews__form form' action='#' method='post'>
+    <form className='reviews__form form' action='#' method='post' onSubmit={handleReviewFormSubmit}>
       <label className='reviews__label form__label' htmlFor='review'>
         Your review
       </label>
